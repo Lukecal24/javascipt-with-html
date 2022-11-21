fdc3eec0df9690f19ac80c63c081dfdec0384c08
Luke Calderon <luke.a.calderon@gmail.com>
2022-11-21 11:44:04 -0600

    console log inline add src to ecternal add alert welcome to website and ask fav color and give response

---
+++ js/external.js
@@ -0,0 +1,8 @@
+console.log("Hello from external Javascript");
+
+alert("Welcome to my Website!");
+
+var userInput = prompt("What's your favorite color?");
+console.log('The user entered: ' + userInput);
+
+alert("My favorite color is " + userInput + " too!!!");
